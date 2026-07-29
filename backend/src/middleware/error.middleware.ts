7a8c42fd3a984a058ae30582b9c822c6b351fbc2
Valerii Oleksiienko <akerman8048@gmail.com>
2026-07-29 10:29:36 +0000

    Implement AppError class in global error middleware

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -1,16 +1,27 @@
+import { AppError } from "@/errors/app-error.js";
 import type { Request, Response, NextFunction } from "express";
 
 export const errorMiddleware = (
-  error:unknown,
+  error: unknown,
   _req: Request,
   res: Response,
   _next: NextFunction,
 ) => {
-  console.error(error)
-  res.status(500).json({
-    error: {
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Something went wrong",
-    },
-  });
+  console.error(error);
+
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      error: {
+        code: error.code,
+        message: error.message,
+      },
+    });
+  } else {
+    res.status(500).json({
+      error: {
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Something went wrong",
+      },
+    });
+  }
 };
